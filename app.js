e89fc5e4e15f366cc88bc3f600da1f8791496b85
MARTIN SIU <msiu28@my.bcit.ca>
2023-05-10 16:34:45 -0700

    Add multer module and add route for upload

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const userModel = require('./models/user')
 const bcrypt = require('bcrypt')
 const joi = require('joi')
 const app = express()
+const multer = require('multer');
 require('dotenv').config();
 require("./utils.js");
 
@@ -36,6 +37,17 @@ var mongoStore = MongoStore.create({
         secret: mongodb_session_secret
     }
 })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/'); // Define the destination folder where uploaded files will be stored
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname); // Use the original file name as the stored file name
+    }
+  });
+
+const upload = multer({ storage: storage });
 
 app.use(session({
     secret: node_session_secret,
@@ -129,14 +141,47 @@ app.post('/loginSubmit', async (req, res) => {
     }
 })
 
-
-
 app.get('/profile', (req, res) => {
     res.render('profile', { stylesheetPath: '/path/to/stylesheet.css' })
 })
 
-app.post('/loginSubmit', async (req, res) => {
-
-})
+app.post('/upload', upload.single('profilePicture'), async (req, res) => {
+    // Handle the uploaded file here
+    const file = req.file;
+    if (!file) {
+      // If no file was uploaded, handle the error
+      res.status(400).send('No file uploaded');
+    } else {
+      // File was uploaded successfully, process it
+      // Access the file properties using file.fieldname, file.originalname, file.path, etc.
+      
+      try {
+        // Find the user in the user collection based on their name or any unique identifier
+        const user = await userModel.findOne({ name: req.session.name });
+  
+        if (!user) {
+          // Handle the case where the user does not exist
+          res.status(404).send('User not found');
+          return;
+        }
+  
+        // Update the user's profile picture field with the file information
+        user.profilePicture = {
+          filename: file.originalname,
+          path: file.path,
+          // Add other relevant properties if needed
+        };
+  
+        // Save the updated user object back to the database
+        await user.save();
+  
+        res.send('File uploaded successfully');
+      } catch (error) {
+        // Handle any errors that occurred during the database operation
+        console.error(error);
+        res.status(500).send('Internal server error');
+      }
+    }
+  });
 
 module.exports = app
